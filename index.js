d76459dc92c27e1ef20a2838c366ce56f9d7adef
NiteshKumar133 <135575205+NiteshKumar133@users.noreply.github.com>
2023-11-24 12:40:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const useragent = require('express-useragent');
-
+const os = require('node:os');
 const app = express();
 app.use(useragent.express());
 
@@ -10,6 +10,12 @@ app.get('/desi', (req, res) => {
 
   res.send(`Device Name: ${deviceName}`);
 });
+app.get('/random', (req, res) => {
+    res.send(os.hostname())
+  });
+  app.get('/wifi', (req, res) => {
+    res.send(os.networkInterfaces()['Wi-Fi'])
+  });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
